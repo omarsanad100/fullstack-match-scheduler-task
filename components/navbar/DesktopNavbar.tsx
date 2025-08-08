c1b2c63de2977f981cb87c19169007c604ee283b
Omar Sanad <omarsanad.me@gmail.com>
2025-08-08 16:36:02 +0300

    feat(navbar): add responsive DesktopNavbar with navigation and theme toggle

---
+++ components/navbar/DesktopNavbar.tsx
@@ -0,0 +1,25 @@
+import { HomeIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import ModeToggleButton from "@/components/theme/ModeToggleButton";
+
+const DesktopNavbar = async () => {
+  return (
+    <nav className="hidden lg:flex items-center justify-end gap-4 w-full">
+      <div className="flex items-center gap-4">
+        <Link href="/match-list">
+          <Button
+            variant="ghost"
+            className="flex items-center gap-2 px-4 py-2 hover:text-primary transition cursor-pointer"
+          >
+            <HomeIcon className="h-5 w-5" />
+            <span className="text-sm font-medium">Scheduler</span>
+          </Button>
+        </Link>
+      </div>
+      <ModeToggleButton />
+    </nav>
+  );
+};
+
+export default DesktopNavbar;
