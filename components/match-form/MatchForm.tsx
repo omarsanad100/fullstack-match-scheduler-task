ea174363f85938f63f2d87946488e64185292a7f
Omar Sanad <omarsanad.me@gmail.com>
2025-08-08 16:37:21 +0300

    feat(match-form): add MatchForm component to handle match creation logic and API integration

---
+++ components/match-form/MatchForm.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from "react";
+import type { Tournament } from "@prisma/client";
+import MatchFormUI from "./MatchFormUI";
+import axios from "axios";
+
+type MatchFormProps = {
+  tournaments: Tournament[];
+};
+
+const MatchForm = ({ tournaments }: MatchFormProps) => {
+  const [formData, setFormData] = useState({
+    teamA: "",
+    teamB: "",
+    matchDate: "",
+    tournamentId: tournaments[0]?.id.toString() || "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await axios.post("/api/matches", formData);
+
+    setFormData({
+      teamA: "",
+      teamB: "",
+      matchDate: "",
+      tournamentId: tournaments[0]?.id.toString() || "",
+    });
+  };
+
+  return (
+    <MatchFormUI
+      tournaments={tournaments}
+      formData={formData}
+      setFormData={setFormData}
+      handleSubmit={handleSubmit}
+    />
+  );
+};
+
+export default MatchForm;
